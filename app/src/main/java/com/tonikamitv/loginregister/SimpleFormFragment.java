558d3d4dbb4b242ee09d9efc46f00148f00a77a8
Julio <Torhque@live.com>
2015-12-05 15:54:55 -0700

    Added the Calendar View gradle info (repository/dependency) Added Simple Form fragment, layout, and arrays.xml required for the Spinner to work in the form

---
+++ app/src/main/java/com/tonikamitv/loginregister/SimpleFormFragment.java
@@ -0,0 +1,27 @@
+package com.tonikamitv.loginregister;
+
+public class SimpleFormFragment {
+
+    // Button click handler for the simple form
+    /*public void reserveATime(View button) {
+        // Do click handling here
+
+        // Reading input from the EditText Controls
+        final EditText startTimeField = (EditText) findViewById(R.id.EditTextStartTime);
+        String startTime = startTimeField.getText().toString();
+
+        final EditText endTimeField = (EditText) findViewById(R.id.EditTextEndtime);
+        String endTime = endTimeField.getText().toString();
+
+        final EditText descriptionField = (EditText) findViewById(R.id.EditTextDescription);
+        String description = descriptionField.getText().toString();
+
+        // Reading input from the Spinner Controls
+        final Spinner kitchenItemSpinner = (Spinner) findViewById(R.id.SpinnerKitchenItem);
+        String kitchenItem = kitchenItemSpinner.getSelectedItem().toString();
+
+        // Reading input from CheckBox Controls
+        final CheckBox servingCheckbox = (CheckBox) findViewById(R.id.CheckBoxServing);
+        boolean bRequiresResponse = servingCheckbox.isChecked();
+    }*/
+}
