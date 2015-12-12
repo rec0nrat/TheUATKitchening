68276c88ab768e65f4bfd296ba469c5def50f921
Julio <Torhque@live.com>
2015-12-11 19:21:00 -0700

    I've manipulated the Simple Form Fragment code, but I feel like I'm missing one more step to properly store the form's information. I don't know what it is though.
    Here is the original Simple Form Tutorial I followed, but it uses the Main Activity for reference.
    http://code.tutsplus.com/tutorials/android-essentials-creating-simple-user-forms--mobile-1758
    
    I found this project that is using a Fragment instead of an Activity, and I tried applying the same concepts to my code. I can't figure out what the missing piece is though.
    http://stackoverflow.com/questions/31929138/how-do-i-findviewbyid-in-android-studio-with-fragments

--- app/src/main/java/com/tonikamitv/loginregister/SimpleFormFragment.java
+++ app/src/main/java/com/tonikamitv/loginregister/SimpleFormFragment.java
@@ -1,27 +1,37 @@
 package com.tonikamitv.loginregister;
 
-public class SimpleFormFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class SimpleFormFragment{
 
     // Button click handler for the simple form
-    /*public void reserveATime(View button) {
-        // Do click handling here
+    public void reserveTime(View button, LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.simple_form_layout, container, false);
 
         // Reading input from the EditText Controls
-        final EditText startTimeField = (EditText) findViewById(R.id.EditTextStartTime);
+        final EditText startTimeField = (EditText) v.findViewById(R.id.EditTextStartTime);
         String startTime = startTimeField.getText().toString();
 
-        final EditText endTimeField = (EditText) findViewById(R.id.EditTextEndtime);
+        final EditText endTimeField = (EditText) v.findViewById(R.id.EditTextEndtime);
         String endTime = endTimeField.getText().toString();
 
-        final EditText descriptionField = (EditText) findViewById(R.id.EditTextDescription);
+        final EditText descriptionField = (EditText) v.findViewById(R.id.EditTextDescription);
         String description = descriptionField.getText().toString();
 
         // Reading input from the Spinner Controls
-        final Spinner kitchenItemSpinner = (Spinner) findViewById(R.id.SpinnerKitchenItem);
+        final Spinner kitchenItemSpinner = (Spinner) v.findViewById(R.id.SpinnerKitchenItem);
         String kitchenItem = kitchenItemSpinner.getSelectedItem().toString();
 
         // Reading input from CheckBox Controls
-        final CheckBox servingCheckbox = (CheckBox) findViewById(R.id.CheckBoxServing);
+        final CheckBox servingCheckbox = (CheckBox) v.findViewById(R.id.CheckBoxServing);
         boolean bRequiresResponse = servingCheckbox.isChecked();
-    }*/
+    }
 }
